2a5d1b68315edd6af80e344112dd9f29d121cbfd
Nino Chimento <saninoc84@gmail.com>
2019-12-16 16:44:43 +0100

    aggiustamento

--- js/myscript.js
+++ js/myscript.js
@@ -1,2 +1,2 @@
-alert('nino');
+alert(" se ami kya premi ok--altrimenti non puoi usare il mio sito perche non sei amico degli animali");
 document.getElementById('sostituation').innerHTML="Nuovo Titolo"
